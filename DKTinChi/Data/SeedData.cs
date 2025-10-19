18d5edd5a02bd4c11227551d5298b9a5e17f4f24
Nguyễn Thái Dương <ntd05102003@gmail.com>
2025-10-19 12:09:19 +0700

    Them file sql add 1 so sinh vien dang ky tin chi

--- DKTinChi/Data/SeedData.cs
+++ DKTinChi/Data/SeedData.cs
@@ -10,7 +10,12 @@ namespace DKTinChi.Data
             {
                 context.SinhViens.AddRange(
                     new Models.modelSinhVien { IdSinhVien = "sv1", HoTen = "Nguyen Van A",Lop = "15A1", MatKhau = "12345" },
-                    new Models.modelSinhVien { IdSinhVien = "sv2", HoTen = "Nguyen Van B", Lop = "15A2", MatKhau = "12345" }
+                    new Models.modelSinhVien { IdSinhVien = "sv2", HoTen = "Nguyen Van B", Lop = "15A2", MatKhau = "12345" },
+                    new Models.modelSinhVien { IdSinhVien = "sv3", HoTen = "Nguyen Van C", Lop = "15A3", MatKhau = "12345" },
+                    new Models.modelSinhVien { IdSinhVien = "sv4", HoTen = "Nguyen Van D", Lop = "15A4", MatKhau = "12345" },
+                    new Models.modelSinhVien { IdSinhVien = "sv5", HoTen = "Nguyen Van E", Lop = "15A5", MatKhau = "12345" },
+                    new Models.modelSinhVien { IdSinhVien = "sv6", HoTen = "Nguyen Van F", Lop = "15A6", MatKhau = "12345" },
+                    new Models.modelSinhVien { IdSinhVien = "sv7", HoTen = "Nguyen Van G", Lop = "15A7", MatKhau = "12345" }
                 );
                 context.SaveChanges();
             }
@@ -18,6 +23,11 @@ namespace DKTinChi.Data
             {
                 context.TinChis.AddRange(
                     new Models.modelTinChi { TenTinChi = "C#", SoTinChi = 4 },
+                    new Models.modelTinChi { TenTinChi = "Hướng đối tượng", SoTinChi = 4 },
+                    new Models.modelTinChi { TenTinChi = "Cơ sở dữ liệu", SoTinChi = 3},
+                    new Models.modelTinChi { TenTinChi = "Lập trình nhúng", SoTinChi = 3 },
+                    new Models.modelTinChi { TenTinChi = "Cấu trúc dữ liệu & giải thuật", SoTinChi = 3 },
+                    new Models.modelTinChi { TenTinChi = "Java", SoTinChi = 4 },
                     new Models.modelTinChi { TenTinChi = "C++", SoTinChi = 4 }
                 );
                 context.SaveChanges();
---
+++ DKTinChi/ThemDuLieu.sql
@@ -0,0 +1,9 @@
+﻿
+insert into DangKys (IdSinhVien, IdTinChi, SoTinDaDangKy, TrangThai) values ('sv2', 1, 4, 1)
+insert into DangKys (IdSinhVien, IdTinChi, SoTinDaDangKy, TrangThai) values ('sv2', 2, 4, 1)
+insert into DangKys (IdSinhVien, IdTinChi, SoTinDaDangKy, TrangThai) values ('sv2', 4, 3, 0)
+insert into DangKys (IdSinhVien, IdTinChi, SoTinDaDangKy, TrangThai) values ('sv3', 1, 4, 1)
+insert into DangKys (IdSinhVien, IdTinChi, SoTinDaDangKy, TrangThai) values ('sv3', 4, 3, 1)
+insert into DangKys (IdSinhVien, IdTinChi, SoTinDaDangKy, TrangThai) values ('sv3', 5, 3, 0)
+insert into DangKys (IdSinhVien, IdTinChi, SoTinDaDangKy, TrangThai) values ('sv4', 2, 4, 1)
+insert into DangKys (IdSinhVien, IdTinChi, SoTinDaDangKy, TrangThai) values ('sv4', 3, 4, 0)
